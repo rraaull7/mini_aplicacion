0c5b116fb79c88433eb7da01061d65dc7efd04ad
rraaull7 <rraaulldemiguel@gmail.com>
2026-02-11 13:20:33 +0100

    hola

--- src/main/java/es/fplumara/dam1/textapp/config/AppConfig.java
+++ src/main/java/es/fplumara/dam1/textapp/config/AppConfig.java
@@ -1,4 +1,51 @@
 package es.fplumara.dam1.textapp.config;
 
+import es.fplumara.dam1.textapp.exceptions.ConfigException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
 public class AppConfig {
+
+    private String storeType;
+    private String messagesFile;
+    private int maxLength;
+
+    public String getStoreType() {
+        return storeType;
+    }
+
+    public String getMessagesFile() {
+        return messagesFile;
+    }
+
+    public int getMaxLength() {
+        return maxLength;
+    }
+    public AppConfig() throws IOException {
+
+        Path path = Path.of("C:\\Users\\RaúlDeMiguelJuan\\aplicacionmini\\data\\config.properties");
+        Properties properties = new Properties();
+        try (InputStream in = Files.newInputStream(path)) {
+            properties.load(in);
+        } catch (IOException e) {
+            throw new ConfigException(" NO SE HA PODIDO LEER CONFIG.PROPERTIES");
+        }
+
+
+
+        storeType = properties.getProperty("store.type");
+        messagesFile = properties.getProperty("messages.file");
+        maxLength = Integer.parseInt(properties.getProperty("messages.MaxLenght"));
+
+
+
+    }
+
+
+
 }
+
