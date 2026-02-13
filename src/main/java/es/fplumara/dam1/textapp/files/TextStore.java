433e596de0934d3f01b467cf4461c938c95e7ae9
rraaull7 <rraaulldemiguel@gmail.com>
2026-02-13 14:13:24 +0100

    messageModel

--- src/main/java/es/fplumara/dam1/textapp/files/TextStore.java
+++ src/main/java/es/fplumara/dam1/textapp/files/TextStore.java
@@ -1,8 +1,14 @@
 package es.fplumara.dam1.textapp.files;
 
+import es.fplumara.dam1.textapp.model.Message;
+
 public interface TextStore {
 
+public  void save (Message message);
 
+public String readAll();
+
+public String readLast(int N);
 
 
 }
